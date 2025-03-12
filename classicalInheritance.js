7eab3ae257486174a91b5fe7a512f73ce3e3ebbc
Mohit Singh <mohitsinghparihar1511@gmail.com>
2025-03-12 20:30:33 +0530

    adding debounce function using setTimeout

--- classicalInheritance.js
+++ classicalInheritance.js
@@ -1,13 +1,19 @@
+// Class Human has gender property.
+
 class Human {
     gender;
     constructor(g) {
+        // provideing initial value to gender
         this.gender = g;
     }
 }
 
+//Class Student extending Human class
 class Student extends Human {
     rollNo;
     constructor(rollNo, gender) {
+        // Providing initial value to rollNo and gender.
+        // As gender property is in the parent calss , we use super keyword to intitialize its value
         super(gender)
         this.rollNo = rollNo;
     }
